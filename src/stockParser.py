fc52626f60e87e57554d16d47368b534fa377d24
TooSchoolForCool <zeyuz@outlook.com>
2017-11-07 10:30:15 -0800

    Add fluctuation features extractor

--- src/stockParser.py
+++ src/stockParser.py
@@ -1,47 +1,83 @@
 #!/usr/local/bin/python3
 
-import csv
+import pandas as pd
+import numpy as np
 
 AAPL_PATH = "../data-set/AAPL.csv"
 GOOG_PATH = "../data-set/GOOG.csv"
 MSFT_PATH = "../data-set/MSFT.csv"
 
+DEBUG = False
+
 class stockParser(object):
 
 	def __init__(self, file_path):
-		self.stock_history_ = []
+		self.data_frame_ = pd.read_csv(file_path)
+		self.size_ = len(self.data_frame_.values)
 
-		with open(file_path, "r") as cvsfile:
-			stock_reader = csv.reader(cvsfile)
+		if DEBUG:
+			self.data_frame_ = self.data_frame_.loc[:10,:]
+			self.size_ = len(self.data_frame_.values)
 
-			next(stock_reader)
-			for row in stock_reader:
-				#  0    1    2    3   4     5         6
-				# data open high low close adj_close volumn
-				self.stock_history_.append([row[0]] + [eval(x) for x in row[1:]])
+			print self.data_frame_
 
-		# Sort list in ascending order with date
-		self.stock_history_ = sorted(self.stock_history_, key=lambda obj:obj[0])
-
-
-	def getData(self):
-		return self.stock_history_
-
-
-	def size(self):
-		return len(self.stock_history_)
-
-
-	def calcFulctuation(self, i):
-		if(i == 0):
+	def getFluctuationVector(self, k):
+		if k < 0:
 			return None
-		else:
-			return (self.stock_history_[i - 1][4] - self.stock_history_[i][4]) / self.stock_history_[i - 1][4]
+
+		# size of fluc_record = size_ - 1, it ripped out the head of data_frame_
+		fluc_record = [self.calcFluctuation(i - 1, i) for i in range(1, self.size_)]
+
+		fluc_vec = []
+		for i in range(0, k):
+			fluc_vec.append(fluc_record[k - i - 1 : len(fluc_record) - i - 1])
+			# fluc_vec.append([fluc_record[j - i] for j in range(k - 1, len(fluc_record) - 1)])
+
+		# convert to dict for DataFrame initialization
+		fluc_vec = {'x'+str(i):fluc_vec[i - 1] for i in range(1, k + 1)}
+
+		# Add label for each vector
+		fluc_vec['y'] = fluc_record[k : ]
+		# fluc_vec['y'] = [fluc_record[i] for i in range(k, len(fluc_record))]
+
+		# Add Date associate with each vector
+		fluc_vec['Date'] = self.data_frame_.loc[k : self.size_ - 2, 'Date'].values
+		# fluc_vec['Date'] = [self.data_frame_.loc[i, 'Date'] for i in range(k, self.size_ - 1)]
+		
+		# convert to Pandas DataFrame
+		fluc_vec = pd.DataFrame(fluc_vec)
+
+		# separate DataFrame into x, y, Date
+		x = fluc_vec.drop(['y', 'Date'], axis=1)
+		y = fluc_vec['y']
+		Date = fluc_vec['Date']
+
+		if DEBUG:
+			print fluc_record
+			print fluc_vec
+			print x
+			print y
+			print Date
+
+		return x, y, Date
+
+
+	def calcFluctuation(self, prev_ix, cur_ix):
+		prev = self.data_frame_.loc[prev_ix, 'Close']
+		cur = self.data_frame_.loc[cur_ix, 'Close']
+
+		return (cur - prev) / prev
+
+
+	def test(self):
+		self.getFluctuationVector(5)
+
+
 
 def main():
 	stock_parser = stockParser(GOOG_PATH)
-	for i in range(1, stock_parser.size()):
-		print(stock_parser.calcFulctuation(i))
+	stock_parser.test()
+	
 
 if __name__ == '__main__':
 	main()
---
+++ src/utils.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+import pandas as pd
+import numpy as np
+
+
+def main():
+	pass
+
+if __name__ == '__main__':
+	main()
