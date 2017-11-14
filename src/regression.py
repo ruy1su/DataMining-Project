d2355e07065ba6ac464e201637a4af799f944fe3
TooSchoolForCool <zeyuz@outlook.com>
2017-11-13 22:01:40 -0800

    Add some comments

--- src/regression.py
+++ src/regression.py
@@ -6,15 +6,47 @@ import numpy as np
 import utils
 import stockParser as sp
 
-
+#############################################################
+# 
+#############################################################
 class regressionModel(object):
+    #############################################################
+    # Constructor
+    # Input:
+    #   model_type:
+    #       0 - linear model; 1 - non-linear model
+    #   gradient_type:
+    #       0 - closed form; 1 - batch gradient descent
+    #   zscore: 
+    #       False - do NOT apply z-score normalization
+    #       True - apply z-score normalization
+    #############################################################
     def __init__(self, model_type, gradient_type=0, zscore=False):
         self.model_type_ = model_type
         self.zscore_ = zscore
         self.gradient_type_ = gradient_type
+
+        # gradient descent learning rate
         self.alpha_ = 0.001
 
-
+    #############################################################
+    # Model Training function
+    # Input:
+    #   x:  
+    #       feature vector data set
+    #       type: Pandas DataFrame (n * p dimension)
+    #       x.values: [[x11, x12, ..., x1p], ..., [xn1, ..., xnp]]
+    #   y:  
+    #       label vector data set
+    #       type: Pandas DataFrame (n * 1 dimension)
+    #       y.values: [[y1], [y2], [y3], ..., [yn]]
+    #
+    # Output:
+    #   self.beta_: 
+    #       weight vector
+    #       type: np.ndarray (p + 1 dimension)
+    #       [b0, b1, b2, ..., bp] 
+    #############################################################
     def train(self, x, y):
         # deep copy
         train_x = x[:]
@@ -39,8 +71,23 @@ class regressionModel(object):
 
         # linear regression
         if(self.model_type_ == 0):
+            # train_x, train_y is DataFrame, convert them into numpy array
             self.beta_ = gradient_func(train_x.values, train_y.values.ravel())
 
+    #############################################################
+    # Model Prediction Function
+    # Input:
+    #   x:  
+    #       feature vector data set
+    #       type: Pandas DataFrame (n * p dimension)
+    #       x.values: [[x11, x12, ..., x1p], ..., [xn1, ..., xnp]]
+    #
+    # Output:
+    #   predicted_y:
+    #       predicted label vector
+    #       type: numpy array (n dimension)
+    #       [y1, y2, y3, ..., yn]
+    #############################################################
     def predict(self, x):
         # deep copy
         test_x = x[:]
@@ -58,7 +105,25 @@ class regressionModel(object):
 
         return predicted_y
 
-
+    #############################################################
+    # Model Testing Function
+    #
+    # This function used K-Fold to test the performance of 
+    # current learning model, and calculate Most Square Error (MSE)
+    # at each iteration.
+    #
+    # Input:
+    #   x:  
+    #       feature vector data set
+    #       type: Pandas DataFrame (n * p dimension)
+    #       x.values: [[x11, x12, ..., x1p], ..., [xn1, ..., xnp]]
+    #   y:  
+    #       label vector data set
+    #       type: Pandas DataFrame (n * 1 dimension)
+    #       y.values: [[y1], [y2], [y3], ..., [yn]]
+    #   k:
+    #       K-Fold parameter
+    #############################################################
     def tester(self, x, y, k):
         mse_sum = 0
 
@@ -68,7 +133,10 @@ class regressionModel(object):
             self.train(train_x, train_y)
 
             predicted_y = self.predict(test_x)
-            
+
+            # computeMSE need input params in excatly same dimension
+            # Here both predicted_y and test_y.values.ravel() is a
+            # n-dimension vector [y1, y2, ..., yn]
             mse = utils.computeMSE(predicted_y, test_y.values.ravel())
             mse_sum += mse
 
@@ -78,7 +146,24 @@ class regressionModel(object):
         print("Average MSE:", mse_sum / k)
         
 
-
+    #############################################################
+    # Using closed form formula to calculate the weight vector beta
+    #
+    # Input:
+    #   train_x:  
+    #       feature vector data set
+    #       type: numpy array (n * p dimension)
+    #       train_x: [[x11, x12, ..., x1p], ..., [xn1, ..., xnp]]
+    #   train_y:  
+    #       label vector data set
+    #       type: numpy array (n dimension)
+    #       train_y: [y1, y2, y3, ..., yn]
+    #
+    # Output:
+    #   beta:
+    #       weight vector (p+1 dimension)
+    #       [b0, b1, b2, ..., bn]
+    #############################################################
     def closedForm(self, train_x, train_y):
         beta = np.zeros(train_x.shape[1])
 
@@ -90,6 +175,24 @@ class regressionModel(object):
         return beta
 
 
+    #############################################################
+    # Using batch gradient descent to calculate the weight vector beta
+    #
+    # Input:
+    #   train_x:  
+    #       feature vector data set
+    #       type: numpy array (n * p dimension)
+    #       train_x: [[x11, x12, ..., x1p], ..., [xn1, ..., xnp]]
+    #   train_y:  
+    #       label vector data set
+    #       type: numpy array (n dimension)
+    #       train_y: [y1, y2, y3, ..., yn]
+    #
+    # Output:
+    #   beta:
+    #       weight vector (p+1 dimension)
+    #       [b0, b1, b2, ..., bn]
+    #############################################################
     def getBetaBatchGradient(self, train_x, train_y):
         beta = np.zeros(train_x.shape[1])
 
@@ -99,11 +202,15 @@ class regressionModel(object):
         while(True):
             hypothesis = np.dot(train_x, beta)
 
+            # calculate the difference between predicted value
+            # and the truth value
             loss = hypothesis - train_y
 
             gradient = np.dot(xTrans, loss)
 
+            # calculate the cost function
             cost = np.sum(loss ** 2) / (2 * train_x.shape[0])
+
             # When cost function does not change, then stop the loop
             if(abs(prev_cost - cost) < 0.00001):
                 break
@@ -119,7 +226,7 @@ def main():
 
     x, y, date = AAPL.getFluctuationVector(5)
 
-    lm = regressionModel(0, gradient_type=1, zscore=True)
+    lm = regressionModel(0, gradient_type=0, zscore=True)
     lm.tester(x, y, 5)
 
 
