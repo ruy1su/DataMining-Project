641a8364608d59ac932324f51e1511f5488f970b
TooSchoolForCool <zeyuz@outlook.com>
2017-11-13 22:45:04 -0800

    Add some comments

--- src/utils.py
+++ src/utils.py
@@ -5,11 +5,21 @@ import numpy as np
 
 from sklearn import cross_validation
 
+# Define the path of stock record file 
 AAPL_PATH = "../data-set/AAPL.csv"
 GOOG_PATH = "../data-set/GOOG.csv"
 MSFT_PATH = "../data-set/MSFT.csv"
 
+
+#######################################################################
+# Apply z-score normalization
+#
+# Input:
+#   dataframe:
+#       Pandas DataFrame, could be any-dimension
+#######################################################################
 def applyZScore(dataframe):
+    print(type(dataframe))
     expectation = np.mean(dataframe, axis=0)
     std_dev = np.std(dataframe, axis=0)
 
@@ -17,15 +27,44 @@ def applyZScore(dataframe):
 
     return normalized_dataframe
 
-# predicted_y and test_y are the predicted and actual y values respectively as numpy arrays
-# function prints the mean squared error value for the test dataset
-# Input: np.ndarray in SAME dimension
-#        [1, 1, 1, 1] vs. [1, 1, 0, 1]
-#     or [[1],[1],[1],[1]] vs. [[1],[1],[0],[1]]
+
+#######################################################################
+# Compute Mean Square Error (MSE)
+#
+# Input:
+#   predicted_y:
+#       predicted label y
+#       numpy ndarray (n dimension or n * 1 dimension)
+#   test_y:
+#       ground truth label y
+#       numpy ndarray (n dimension or n * 1 dimension)
+# 
+# Output:
+#   MSE: Mean Squre Error
+#
+# Note:
+#   predicted_y and test_y should be in the SAME dimension
+#   e.g., [1, 1, 1, 1] vs. [1, 1, 0, 1] 
+#      or [[1],[1],[1],[1]] vs. [[1],[1],[0],[1]]
+#######################################################################
 def computeMSE(predicted_y, test_y):
     mse = np.sum((predicted_y - test_y) ** 2) / predicted_y.shape[0]
     return mse
 
+
+#######################################################################
+# Compute Recall - TP / (TP + FN)
+#
+# Input:
+#   predicted_y:
+#       predicted label vector
+#       type: numpy array (n dimension)
+#       [y1, y2, y3, ..., yn]
+#   test_y:
+#       ground truth label vector
+#       type: numpy array (n dimension)
+#       [y1, y2, y3, ..., yn]
+#######################################################################
 def computeRecall(predicted_y, test_y):
     TP, FN = 0, 0
 
@@ -39,6 +78,20 @@ def computeRecall(predicted_y, test_y):
 
     return ret
 
+
+#######################################################################
+# Compute Precision - TP / (TP + FP)
+#
+# Input:
+#   predicted_y:
+#       predicted label vector
+#       type: numpy array (n dimension)
+#       [y1, y2, y3, ..., yn]
+#   test_y:
+#       ground truth label vector
+#       type: numpy array (n dimension)
+#       [y1, y2, y3, ..., yn]
+#######################################################################
 def computePrecision(predicted_y, test_y):
     TP, FP = 0, 0
 
@@ -52,6 +105,20 @@ def computePrecision(predicted_y, test_y):
 
     return ret
 
+
+#######################################################################
+# Compute Recall - (TP + TN) / ALL
+#
+# Input:
+#   predicted_y:
+#       predicted label vector
+#       type: numpy array (n dimension)
+#       [y1, y2, y3, ..., yn]
+#   test_y:
+#       ground truth label vector
+#       type: numpy array (n dimension)
+#       [y1, y2, y3, ..., yn]
+#######################################################################
 def computeAccuracy(predicted_y, test_y):
     TP, FP, TN, FN = 0, 0, 0, 0
 
@@ -69,16 +136,52 @@ def computeAccuracy(predicted_y, test_y):
 
     return ret
 
-# sign function, convert continuous value into two classes [0, 1]
-# Input vec is a DataFrame
-# Return a new DataFrame with each item applied sign function
+
+#######################################################################
+# sign function
+#   y = 1 if x >= 0, otherwise y = 0
+# 
+# Input:
+#   vec: Pandas DataFrame in any dimension (Cannot be Series)
+#######################################################################
 def sign(vec):
     # value of vec will not be changed
     new_vec = vec.applymap(lambda x: 1 if x >= 0 else 0)  
 
     return new_vec
 
-# Perform K-Fold testing
+
+#######################################################################
+# K-Fold Dataset Generator
+#   This function will generate training set and testing set for
+#   k-fold cross validation
+#
+# Input:
+#   x:  
+#       feature vector data set
+#       type: Pandas DataFrame (n * p dimension)
+#       x.values: [[x11, x12, ..., x1p], ..., [xn1, ..., xnp]]
+#   y:  
+#       label vector data set
+#       type: Pandas DataFrame (n * 1 dimension)
+#       y.values: [[y1], [y2], [y3], ..., [yn]]
+#   k:
+#       K-Fold parameter (how any fold?)
+#
+# Output: A zip of (ret_train_x, ret_train_y, ret_test_x, ret_test_y)
+#   ret_train_x: training feature vectors set
+#       type: Pandas DataFrame (n * p dimension)
+#       x.values: [[x11, x12, ..., x1p], ..., [xn1, ..., xnp]]
+#   ret_train_y: training label vectors set
+#       type: Pandas DataFrame (n * 1 dimension)
+#       y.values: [[y1], [y2], [y3], ..., [yn]]
+#   ret_test_x: testing feature vectors set
+#       type: Pandas DataFrame (n * p dimension)
+#       x.values: [[x11, x12, ..., x1p], ..., [xn1, ..., xnp]]
+#   ret_test_y: testing label vectors set
+#       type: Pandas DataFrame (n * 1 dimension)
+#       y.values: [[y1], [y2], [y3], ..., [yn]]
+#######################################################################
 def KfoldGenerator(x, y, k):
     cv = cross_validation.KFold(len(x), n_folds = k)
 
