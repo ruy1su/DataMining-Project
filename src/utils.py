8a85f76322fc61fc67df2d8459a060e67d5c352c
TooSchoolForCool <zeyuz@outlook.com>
2017-11-16 23:50:11 -0800

    Add nextDay function

--- src/utils.py
+++ src/utils.py
@@ -214,6 +214,25 @@ def KfoldGenerator(x, y, k):
     return zip(ret_train_x, ret_train_y, ret_test_x, ret_test_y)
 
 
+def nextDay(date="", year=0, month=0, day=0):
+    if(date != ""):
+        year = eval(date[:4])
+        month = eval(date[6] if date[5] == '0' else date[5:7])
+        day = eval(date[9] if date[8] == '0' else date[8:])
+
+    month_days = [[31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31],
+                  [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]]
+
+    # if is_leap == 0, indicates current year is not leap year
+    is_leap = 0 if (year % 4 != 0 or (year % 100 == 0 and year % 400 != 0)) else 1
+
+    # update day, month, year
+    day = 1 if (month_days[is_leap][month - 1] == day) else day + 1
+    month = 1 if (day == 1 and month == 12) else (month + 1 if (day == 1) else month)
+    year = year + 1 if (month == 1 and day == 1) else year
+
+    return ("%04d-%02d-%02d" % (year, month, day))
+
 def main():
     pass
 
