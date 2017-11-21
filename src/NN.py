6634176a8c7e13814b8e3085d5a7bc7222335338
Shunji Zhan <shunhahaha@live.com>
2017-11-20 20:58:03 -0800

    NN works

--- src/NN.py
+++ src/NN.py
@@ -8,17 +8,19 @@ import random
 
 class NN:
     def __init__(self, X, Y, stepSize=0.1, activation_function=None, hiddenLayers=1, layerNodes=10):
-        self.X = X
-        self.Y = Y
+        self.X = X    # X data (n, d)
+        self.Y = Y    # Y data (n, 1)
+        self.N = X.shape[0]         # number of data points
+        self.D = X.shape[1]         # dimension of each date point
         self.stepSize = stepSize
         self.activation_function = activation_function
         # placeholders for inputs
-        self.x = tf.placeholder(tf.float32, [None, 1])
-        self.y = tf.placeholder(tf.float32, [None, 1])
+        self.x = tf.placeholder(tf.float32, shape=(self.N, self.D))
+        self.y = tf.placeholder(tf.float32, shape=(self.N, 1))
 
         # define layers
         assert(hiddenLayers > 0)
-        layer = self.add_layer(self.x, 1, layerNodes)   # input layer
+        layer = self.add_layer(self.x, self.D, layerNodes)   # input layer
         for i in range(hiddenLayers):
             layer = self.add_layer(layer, layerNodes, layerNodes)   # add hidden layers
         output = self.add_layer(layer, layerNodes, 1)
@@ -34,7 +36,7 @@ class NN:
         for i in range(iterations):
             # training train_step 和 loss 都是由 placeholder 定义的运算，所以这里要用 feed 传入参数
             self.sess.run(self.train_step, feed_dict={self.x: self.X, self.y: self.Y})
-            if i % 100 == 0:
+            if i % 1000 == 0:
                 self.predict()
 
     def predict(self):
@@ -54,18 +56,28 @@ class NN:
 
 
 def f(X):
-    return np.square(X) + 0.53
+    beta = np.random.rand(X.shape[1], 1)
+    return X.dot(beta)
 
 
 if __name__ == '__main__':
-    # X = np.random.rand(300, 5)
-    X = np.linspace(-1, 1, 300)[:, np.newaxis]
+    X = np.random.rand(200, 5)
+    # X = np.linspace(-1, 1, 10)[:, np.newaxis]
     Y = f(X)
-    Y += np.random.normal(-0.05, 0.05, Y.shape)     # add noise
+    Y += np.random.normal(-0.01, 0.01, Y.shape)     # add noise
 
+    print X.shape, Y.shape
+
+    nn = NN(X, Y, stepSize=0.1, activation_function=tf.sigmoid, hiddenLayers=2, layerNodes=10)
+    nn.train(3000)
+
+    print('---------')
     nn = NN(X, Y, stepSize=0.1, activation_function=tf.sigmoid, hiddenLayers=5, layerNodes=30)
     nn.train(3000)
 
+    print('---------')
+    nn = NN(X, Y, stepSize=0.1, activation_function=tf.sigmoid, hiddenLayers=10, layerNodes=50)
+    nn.train(3000)
 
 
 
