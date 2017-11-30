5d6d0281fb0cff0bc99d54702813e2fe2a4bbe9c
Shunji Zhan <shunhahaha@live.com>
2017-11-29 23:19:54 -0800

    added NN test datas

--- src/Plotter.py
+++ src/Plotter.py
@@ -1,12 +1,20 @@
 import matplotlib.pyplot as plt
 
+# def f(x):
+#     return float(format(float(x), '.3f'))
+
+# def precise(arr):
+#     result = list(map(f, arr))
+#     print(result)
+#     return result
+
 class Plotter:
     @staticmethod
     def plot(predict, real):
-        # print(predict[:10])
-        # print(real[:10])
-        plt.xlabel('day i')
-        plt.ylabel('fluctuation')
-        plt.plot(predict[:30], 'b', label='predict')
-        plt.plot(real[:30], 'r', label='real')
-        plt.show()
+        print(predict[:50])
+        print(real[:50])
+        # plt.xlabel('day')
+        # plt.ylabel('fluctuation')
+        # plt.plot(predict[:30], 'b', label='predict')
+        # plt.plot(real[:30], 'r', label='real')
+        # plt.show()
--- src/TensorFlowNN.py
+++ src/TensorFlowNN.py
@@ -15,7 +15,7 @@ class TensorFlowNN:
         self.hiddenLayers = hiddenLayers
         self.layerNodes = layerNodes
 
-    def train(self, X, Y, iterations=3000):
+    def train(self, X, Y, iterations=10000000):
         # X data (n, d)
         # Y data (n, 1)
         D = X.shape[1]         # dimension of each date point
@@ -31,22 +31,23 @@ class TensorFlowNN:
         self.output = self.add_layer(layer, self.layerNodes, 1)
 
         # define loss and train process
-        self.loss = tf.reduce_mean(tf.reduce_sum(tf.square(self.y - self.output), reduction_indices=[1]))
+        self.loss = tf.reduce_mean(tf.reduce_sum(tf.square(self.y - self.output), axis=[1]))
         self.train_step = tf.train.GradientDescentOptimizer(self.stepSize).minimize(self.loss)
 
         init = tf.global_variables_initializer()
         self.sess = tf.Session()
         self.sess.run(init)
 
-        print('----------------------------')
+        print('----------------------------------------')
+        self.print_self_info()
         for i in range(iterations):
             self.sess.run(self.train_step, feed_dict={self.x: X, self.y:Y})
-            if (i + 1) % 1000 == 0:
+            if (i + 1) % 10000 == 0:
                 loss = self.sess.run(self.loss, feed_dict={self.x: X, self.y: Y})
                 print('iter: ' + str(int((i + 1)/1000)) + 'k | err: ' + str(loss))
 
         Plotter.plot(self.predict(X), Y)
-        print('----------------------------\n')
+        print('----------------------------------------\n')
 
     def predict(self, X):
         predict = self.sess.run(self.output, feed_dict={self.x: X})
@@ -54,8 +55,8 @@ class TensorFlowNN:
 
     # add one more layer and return the output of this layer
     def add_layer(self, inputs, in_size, out_size):
-        Weights = tf.Variable(tf.random_normal([in_size, out_size]))
-        biases = tf.Variable(tf.zeros([1, out_size]) + 0.1)
+        Weights = tf.Variable(tf.random_normal(shape=(in_size, out_size)))
+        biases = tf.Variable(tf.zeros(shape=(1, out_size)) + 0.01)
         Wx_plus_b = tf.matmul(inputs, Weights) + biases
         if self.activation_function is None:
             outputs = Wx_plus_b
@@ -63,34 +64,50 @@ class TensorFlowNN:
             outputs = self.activation_function(Wx_plus_b)
         return outputs
 
+    def print_self_info(self):
+        print('step: ' + str(self.stepSize) 
+          + ' layers: ' + str(self.hiddenLayers) 
+          + ' layerNodes: ' + str(self.layerNodes) 
+          + ' activation: ' + str(self.activation_function))
+
 
 def f(X):
     beta = np.random.rand(X.shape[1], 1)
-    return X.dot(beta)
+    X_beta = X.dot(beta)
+    return X_beta / np.linalg.norm(X_beta)
 
 
 if __name__ == '__main__':
-    # X = np.random.rand(1000, 7)
-    # # X = np.linspace(-1, 1, 10)[:, np.newaxis]
-    # Y = f(X)
-    # Y += np.random.normal(-0.01, 0.01, Y.shape)     # add noise
+    # if True:
+    #     X = np.random.rand(50, 10)
+    #     Y = f(X)
+    #     # Y += np.random.normal(-0.01, 0.01, Y.shape)     # add noise
+    #     nn = TensorFlowNN(stepSize=0.01, activation_function=tf.tanh, hiddenLayers=3, layerNodes=3)
+    #     nn.train(X, Y)
 
-    if True:
-        extractor = fe.featureExtractor(0)
-        x, y, date = extractor.getFeature(5, 0)
-        nn1 = TensorFlowNN(stepSize=0.1, activation_function=tf.sigmoid, hiddenLayers=3, layerNodes=5)
-        ts = tester.Tester(2)
-        ts.test(nn1, x, y, 0)
+    extractor = fe.featureExtractor(0)
+    x, y, date = extractor.getFeature(5, 0)
+    nn1 = TensorFlowNN(stepSize=0.0001, activation_function=tf.tanh, hiddenLayers=1, layerNodes=2)
+    ts = tester.Tester(2)
+    ts.test(nn1, x, y, 0)
 
+# iteration: 300k
+# step: 0.01 layers: 2  layerNodes: 2  err: 0.000237969
+# step: 0.01 layers: 3  layerNodes: 3  err: 0.00024034
+# step: 0.01 layers: 4  layerNodes: 4  err: 0.000227995
+# step: 0.01 layers: 5  layerNodes: 5  err: 0.000229507
+# step: 0.1  layers: 2  layerNodes: 2  err: 0.000208698
+# step: 0.1  layers: 3  layerNodes: 3  err: 0.000210886
+# step: 0.1  layers: 4  layerNodes: 4  err: 0.000234041
+# step: 0.1  layers: 5  layerNodes: 5  err: 0.000203047
+# step: 0.1  layers: 10 layerNodes: 10 err: 0.000247254
 
-
-
-
-
-
-
-
-
-
-
+# iteration: 1000k
+# step: 0.0005 layers: 2  layerNodes: 5   err: 0.000277568
+# step: 0.001  layers: 2  layerNodes: 2   err: 0.000190092
+# step: 0.001  layers: 10 layerNodes: 10  err: 0.00124087
+# step: 0.01   layers: 10 layerNodes: 10  err: 0.000175113
+# step: 0.001  layers: 1  layerNodes: 2   err: 0.000256699
+# step: 0.001  layers: 1  layerNodes: 5   err: 0.000205079
+# step: 0.001  layers: 1  layerNodes: 10  err: 0.000301821
 
